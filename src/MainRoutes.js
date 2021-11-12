1178054903e3a8275f35c0935d1620197576966b
Matthew <bekievbeil@gmail.com>
2021-11-12 16:42:02 +0600

    basic structure

---
+++ src/MainRoutes.js
@@ -0,0 +1,16 @@
+import React from 'react'
+
+const MainRoutes = () => {
+  return (
+    <BrowserRouter>
+      <Header />
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/cart" element={<Cart />} />
+        <Route path="/product/:id" element={<ProductDetails />} />
+      </Routes>
+    </BrowserRouter>
+  )
+}
+
+export default MainRoutes
