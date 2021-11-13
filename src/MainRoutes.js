84fffa2b6f1a97f676dbb2fa37237adc1508aba3
Matthew <bekievbeil@gmail.com>
2021-11-13 15:52:41 +0600

    changed buttons

--- src/MainRoutes.js
+++ src/MainRoutes.js
@@ -2,6 +2,7 @@ import React from 'react'
 import { BrowserRouter, Route, Routes } from 'react-router-dom'
 import Auth from './components/Auth/Auth'
 import Cart from './components/Cart/Cart'
+import Header from './components/Header/Header'
 import Home from './components/Home/Home'
 import ProductDetails from './components/Products/ProductDetails'
 import AuthContextProvider from './Contexts/AuthContext'
@@ -10,6 +11,7 @@ const MainRoutes = () => {
   return (
     <AuthContextProvider>
       <BrowserRouter>
+        <Header />
         <Routes>
           <Route path="/" element={<Home />} />
           <Route path="/auth" element={<Auth />} />
