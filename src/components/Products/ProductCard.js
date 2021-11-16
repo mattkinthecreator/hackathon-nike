14c1b19a6c9a918ac41aa2749b14da24e4e5ea05
Matthew <bekievbeil@gmail.com>
2021-11-16 16:40:24 +0600

    something

--- src/components/Products/ProductCard.js
+++ src/components/Products/ProductCard.js
@@ -1,8 +1,9 @@
-import React, {useContext} from 'react';
+import React, { useContext } from 'react'
 import './Product.css'
 import { productsContext } from '../../Contexts/ProductsContext'
 
 const ProductCard = ({ item }) => {
+  const { addProductToCart } = useContext(productsContext)
   return (
     <div class="card">
       <img src={item.images[0].images} width="450px" height="400px" />
@@ -12,26 +13,10 @@ const ProductCard = ({ item }) => {
         <p>color: {item.images.map((elem) => elem.color)}</p>
         <p>size: {item.size.map((item) => item + ' ')} </p>
         <p>Price: {item.price}$</p>
+        <button onClick={() => addProductToCart(item)}>Korzina</button>
       </div>
     </div>
   )
 }
 
-const ProductCard = ( {item} ) => {
-    const { addProductToCart } = useContext(productsContext)
-    return (
-            <div class="card">
-                <img src={item.images[0].images} width="450px" height="400px"/>
-                <div class="container">
-                    <h2>{item.title}</h2>
-                    <p>Category: {item.category}</p>
-                    <p>color: {item.images.map(elem => elem.color)}</p>
-                    <p>size: {item.size.map(item => item + ' ')} </p>
-                    <p>Price: {item.price}$</p>
-                    <button onClick={() => addProductToCart(item)}>Korzina</button>
-                </div>
-            </div>
-    )
-};
-
-export default ProductCard;
+export default ProductCard
--- src/helpers/cartFunction.js
+++ src/helpers/cartFunction.js
@@ -1,28 +1,28 @@
 export function getCountProductsInCart() {
-    let cart = JSON.parse(localStorage.getItem('cart'))
-    return cart ? cart.shoes.length : 0
-  }
-  
-  export function calcSubPrice(shoes) {
-    return shoes.count * shoes.item.price
-  }
-  
-  export function calcTotalPrice(products) {
-    let totalPrice = 0
-    products.forEach((item) => {
-      totalPrice += item.subPrice
-    })
-    return totalPrice
-  }
-  
-  export function checkProductInCart(id) {
-    let cart = JSON.parse(localStorage.getItem('cart'))
-    if (!cart) {
-      cart = {
-        products: [],
-        totalPrice: 0,
-      }
+  const cart = JSON.parse(localStorage.getItem('cart'))
+  return cart ? cart.shoes.length : 0
+}
+
+export function calcSubPrice(shoes) {
+  return shoes.count * shoes.item.price
+}
+
+export function calcTotalPrice(products) {
+  let totalPrice = 0
+  products.forEach((item) => {
+    totalPrice += item.subPrice
+  })
+  return totalPrice
+}
+
+export function checkProductInCart(id) {
+  let cart = JSON.parse(localStorage.getItem('cart'))
+  if (!cart) {
+    cart = {
+      products: [],
+      totalPrice: 0,
     }
-    let newCart = cart.shoes.filter((elem) => elem.item.id === id)
-    return newCart.length > 0 ? true : false
   }
+  let newCart = cart.shoes.filter((elem) => elem.item.id === id)
+  return newCart.length > 0 ? true : false
+}
