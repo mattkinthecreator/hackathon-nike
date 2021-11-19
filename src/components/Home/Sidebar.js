498bbcfd64191e87f62eab8f0902d9137eae10ea
Matthew <bekievbeil@gmail.com>
2021-11-19 15:05:53 +0600

    asd

--- src/components/Home/Sidebar.js
+++ src/components/Home/Sidebar.js
@@ -1,22 +1,22 @@
-import React, { useContext, useState } from 'react';
-import { productsContext } from '../../Contexts/ProductsContext';
-import history from "../../helpers/history";
-import '../Home/Category/Category.css';
+import React, { useContext, useState } from 'react'
+import { productsContext } from '../../Contexts/ProductsContext'
+import history from '../../helpers/history'
+import '../Home/Category/Category.css'
 import './Sidebar.css'
-import cx from "classnames";
-import { CSSTransition } from 'react-transition-group';
+import cx from 'classnames'
+import { CSSTransition } from 'react-transition-group'
 import Slider from './Slider'
 
 const menuItems = [
-    { title: "Спортивный стиль"},
-    { title: "Jordan" },
-    { title: "Бег" },
-    { title: "Футбол" },
-    { title: "Бейсбол"},
-    { title: "Все"}
-  ];
+  { title: 'Спортивный стиль' },
+  { title: 'Jordan' },
+  { title: 'Бег' },
+  { title: 'Футбол' },
+  { title: 'Бейсбол' },
+  { title: 'Все' },
+]
 const Sidebar = () => {
-  const [isOpen, setIsOpen] = useState(true);
+  const [isOpen, setIsOpen] = useState(true)
   const { getProducts } = useContext(productsContext)
   const [category, setCategory] = useState(getCatgeory())
 
@@ -27,11 +27,11 @@ const Sidebar = () => {
 
   console.log(category)
   function handleChangeCategory(value) {
-      if(value === 'Все') {
-          history.push(`${history.location.pathname.replace('category')}`)
-          getProducts()
-          return
-      }
+    if (value === 'Все') {
+      history.push(`${history.location.pathname.replace('category')}`)
+      getProducts()
+      return
+    }
     let search = new URLSearchParams(history.location.search)
     search.set('category', value)
     history.push(`${history.location.pathname}?${search.toString()}`)
@@ -40,34 +40,40 @@ const Sidebar = () => {
   }
   return (
     <>
-    <div className="sidebar__buttons">
-      <button className={"sidebar__button"} onClick={() => setIsOpen(!isOpen)}>
-      фильтры
-   </button>
-    </div>
-    <div className={cx("sidebar", { "sidebar-closed": !isOpen })}>
-      <ul className="sidebar__title">
-        <div className="sidebar__slider">
-          <Slider />
-        </div>
-        {menuItems.map((item) => (
-          <li key={item.title} onClick={() => handleChangeCategory(item.title)}>
-            <div className={"sidebar__listItem"}>
-              <CSSTransition
-                in={isOpen}
-                classNames={"fade"}
-                timeout={1000}
-                unmountOnExit
-              >
-                <span>{item.title}</span>
-              </CSSTransition>
-            </div>
-          </li>
-        ))}
-      </ul>
-    </div>
-  </>
-  );
-};
+      <div className="sidebar__buttons">
+        <button
+          className={'sidebar__button'}
+          onClick={() => setIsOpen(!isOpen)}
+        >
+          фильтры
+        </button>
+      </div>
+      <div className={cx('sidebar', { 'sidebar-closed': !isOpen })}>
+        <ul className="sidebar__title">
+          <div className="sidebar__slider">
+            <Slider />
+          </div>
+          {menuItems.map((item) => (
+            <li
+              key={item.title}
+              onClick={() => handleChangeCategory(item.title)}
+            >
+              <div className={'sidebar__listItem'}>
+                <CSSTransition
+                  in={isOpen}
+                  classNames={'fade'}
+                  timeout={1000}
+                  unmountOnExit
+                >
+                  <span>{item.title}</span>
+                </CSSTransition>
+              </div>
+            </li>
+          ))}
+        </ul>
+      </div>
+    </>
+  )
+}
 
-export default Sidebar;
+export default Sidebar
--- src/components/Products/ProductCard.js
+++ src/components/Products/ProductCard.js
@@ -3,13 +3,9 @@ import './Product.css'
 import { productsContext } from '../../Contexts/ProductsContext'
 import { authContext } from '../../Contexts/AuthContext'
 import { Link } from 'react-router-dom'
-<<<<<<< HEAD
 import AddShoppingCart from '@mui/icons-material/AddShoppingCart'
-import FavoriteBorderIcon from '@mui/icons-material/FavoriteBorder';
-import DetailsIcon from '@mui/icons-material/Details';
-=======
-import { authContext } from '../../Contexts/AuthContext'
->>>>>>> main
+import FavoriteBorderIcon from '@mui/icons-material/FavoriteBorder'
+import DetailsIcon from '@mui/icons-material/Details'
 
 const ProductCard = ({ item }) => {
   const { addProductToCart } = useContext(productsContext)
@@ -17,13 +13,8 @@ const ProductCard = ({ item }) => {
 
   return (
     <div class="card">
-<<<<<<< HEAD
-      <img src={item.images[0].images} width="450px" height="400px" />
-      <div class="container-productCard">
-=======
       <img src={item.images[0].images[0]} width="450px" height="400px" />
       <div class="container">
->>>>>>> main
         <h2>{item.title}</h2>
         <p>{item.category}</p>
         <p>{item.images.map((elem) => elem.color)}</p>
@@ -31,10 +22,15 @@ const ProductCard = ({ item }) => {
         <p>{item.price}$</p>
         <div className="card_button">
           <Link to={`shoes/${item.id}`}>
-            <DetailsIcon/>
+            <DetailsIcon />
           </Link>
-          <span onClick={() => addProductToCart(item)}><AddShoppingCart/></span>
-          <span onClick={() => addProductToFavorites(item)}> <FavoriteBorderIcon/></span>
+          <span onClick={() => addProductToCart(item)}>
+            <AddShoppingCart />
+          </span>
+          <span onClick={() => addProductToFavorites(item)}>
+            {' '}
+            <FavoriteBorderIcon />
+          </span>
         </div>
       </div>
     </div>
